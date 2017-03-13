5c9aa87e5a320f57fd250cec6e6fd700fe2b5a43
pedrocrisp <pedrocrisp@users.noreply.github.com>
2017-03-13 17:51:24 -0500

    Update seqcap_pipeline_test_v0.1.sh

--- SeqCap/seqcap_pipeline_test_v0.1.sh
+++ SeqCap/seqcap_pipeline_test_v0.1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=01:00:00,nodes=1:ppn=1,mem=5gb
-#PBS -o /scratch.global/pcrisp/SeqCap_1_Mei/testpipeline_SeqCap_1_Mei/logs/20170313_testpipeline_SeqCap_1_Mei_o
-#PBS -e /scratch.global/pcrisp/SeqCap_1_Mei/testpipeline_SeqCap_1_Mei/logs/20170313_testpipeline_SeqCap_1_Mei_e
+#PBS -o /scratch.global/pcrisp/SeqCap_1_Mei/testpipeline_SeqCap_1_Mei/analysis/logs/20170313_testpipeline_SeqCap_1_Mei_o
+#PBS -e /scratch.global/pcrisp/SeqCap_1_Mei/testpipeline_SeqCap_1_Mei/analysis/logs/20170313_testpipeline_SeqCap_1_Mei_e
 #PBS -N 20170313_testpipeline_SeqCap_1_Mei
 #PBS -r n
 #PBS -M pcrisp@umn.edu
