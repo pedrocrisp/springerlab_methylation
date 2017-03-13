f540dc206190c2e4b255f4123e4bec64277938bd
pedrocrisp <pedrocrisp@users.noreply.github.com>
2017-03-13 12:26:34 -0500

    Create seqcap_pipeline_test_v0.1.sh

---
+++ SeqCap/seqcap_pipeline_test_v0.1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#PBS -l walltime=01:00:00,nodes=1:ppn=1,mem=5gb
+#PBS -o /scratch.global/pcrisp/SeqCap_1_Mei/testpipeline_SeqCap_1_Mei/logs/20170313_testpipeline_SeqCap_1_Mei_o
+#PBS -e /scratch.global/pcrisp/SeqCap_1_Mei/testpipeline_SeqCap_1_Mei/logs/20170313_testpipeline_SeqCap_1_Mei_e
+#PBS -N 20170313_testpipeline_SeqCap_1_Mei
+#PBS -r n
