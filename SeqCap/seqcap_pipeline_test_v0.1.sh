cc063926eb3f9869a8be9bda63962162dc622e3b
pedrocrisp <pedrocrisp@users.noreply.github.com>
2017-03-14 13:17:39 -0500

    Update seqcap_pipeline_test_v0.1.sh

--- SeqCap/seqcap_pipeline_test_v0.1.sh
+++ SeqCap/seqcap_pipeline_test_v0.1.sh
@@ -55,11 +55,12 @@ cd $workingdir
         # -q 20: trim to q20
         
         #-o "${alignfolder}/${ID}.bam" \
+        
         bsmap \
         -a "${trimmedfolder}/${ID}_R1_001_val_1.fq" \
         -b "${trimmedfolder}/${ID}_R2_001_val_2.fq" \
         -d "${refdir}/Zea_mays.AGPv4.dna.toplevel.fa" \
-        -o ./${ID}.bam \
+        -o "./${ID}.bam" \
         -v 5 \
         -r 0 \
         -p 8 \
