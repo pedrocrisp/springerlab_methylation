4d6013dfbb12a5f1fff3d20a9803a4ad70ccfda6
pedrocrisp <pedrocrisp@users.noreply.github.com>
2017-03-15 00:57:04 -0500

    Update bsmap_qsub.sh

--- SeqCap/bsmap_qsub.sh
+++ SeqCap/bsmap_qsub.sh
@@ -39,28 +39,14 @@ timestamp=$(date +%Y%m%d-%H%M%S)
 workingdir=/scratch.global/pcrisp/SeqCap_1_Mei/testpipeline_SeqCap_1_Mei
 
 analysis_dir=/scratch.global/pcrisp/SeqCap_1_Mei/testpipeline_SeqCap_1_Mei/analysis
-mkdir $analysis_dir
 
-log_folder=${analysis_dir}/logs_${timestamp}
+log_folder=${analysis_dir}/logs_bsmap_${timestamp}
 mkdir $log_folder
 
-fastqcfolder=${analysis_dir}/fastqc
-mkdir $fastqcfolder
-
 trimmed=${analysis_dir}/trimmed
-mkdir $trimmed
 
 alignfolder=${analysis_dir}/bsmaped
 mkdir $alignfolder
-
-BSMAPratio_folder=${analysis_dir}/BSMAPratio
-mkdir $BSMAPratio_folder
-
-TempOut=${analysis_dir}/TempOut
-mkdir $TempOut
-
-OnTargetCoverage=${analysis_dir}/OnTargetCoverage
-mkdir $OnTargetCoverage
 
 #script path and cat a record of what was run
 script_dir=/home/springer/pcrisp/gitrepos/springerlab_methylation/SeqCap
@@ -73,7 +59,7 @@ cat $0 > "$log_folder/runner.log"
 qsub -t $qsub_t \
 -o ${log_folder}/testpipeline_SeqCap_1_Mei_o \
 -e ${log_folder}/testpipeline_SeqCap_1_Mei_e \
--v LIST=${workingdir}/${sample_list},readsdir=${workingdir}/reads,workingdir=$analysis_dir,log_folder=$log_folder,fastqcfolder=$fastqcfolder,trimmedfolder=$trimmed,alignfolder=$alignfolder,BSMAPratio_folder=$BSMAPratio_folder,TempOut=$TempOut,OnTargetCoverage=$OnTargetCoverage \
+-v LIST=${workingdir}/${sample_list},readsdir=${workingdir}/reads,workingdir=$analysis_dir,log_folder=$log_folder,trimmedfolder=$trimmed,alignfolder=$alignfolder \
 $script_to_qsub
 
     #-wd $pbs_pwd \
