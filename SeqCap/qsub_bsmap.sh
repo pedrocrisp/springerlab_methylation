4d357836c6057230c49394f70f73f9c270a353c6
pedrocrisp <pedrocrisp@users.noreply.github.com>
2017-03-15 10:41:42 -0500

    Update qsub_bsmap.sh

--- SeqCap/qsub_bsmap.sh
+++ SeqCap/qsub_bsmap.sh
@@ -8,6 +8,8 @@
 set -x
 set -e
 
+module load samtools
+
 cd /scratch.global/pcrisp/SeqCap_1_Mei/testpipeline_SeqCap_1_Mei/analysis
 
 bsmap -a trimmed/F1-16_Index5_S1_R1_001_val_1.fq -b trimmed/F1-16_Index5_S1_R2_001_val_2.fq -d /home/springer/pcrisp/ws/refseqs/maize/Zea_mays.AGPv4.dna.toplevel.fa -o bsmaped/F1-16_Index5_S1/F1-16_Index5_S1.bam -v 5 -r 0 -p 8 -q 20 -A AGATCGGAAGAGCGGTTCAGCAGGAATGCCG
