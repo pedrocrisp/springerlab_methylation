f54616d846d87e5c2e6cb63e24974f72f51339c4
pedrocrisp <pedrocrisp@users.noreply.github.com>
2017-03-15 09:38:39 -0500

    Update bsmap.sh

--- SeqCap/bsmap.sh
+++ SeqCap/bsmap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l walltime=01:00:00,nodes=1:ppn=8,mem=16gb
+#PBS -l walltime=01:00:00,nodes=1:ppn=1,mem=64gb
 #PBS -N 20170313_testpipeline_SeqCap_1_Mei
 #PBS -r n
 #PBS -m abe
@@ -44,7 +44,7 @@ cd $workingdir
         -o "${alignfolder}/${ID}/${ID}.bam" \
         -v 5 \
         -r 0 \
-        -p 8 \
+        -p 1 \
         -q 20 \
         -A AGATCGGAAGAGCGGTTCAGCAGGAATGCCG
 
