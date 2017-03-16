5cb6086b88cca0f30013043836c482557e1a515b
pedrocrisp <pedrocrisp@users.noreply.github.com>
2017-03-16 12:17:10 -0500

    Update bsmap.sh

--- SeqCap/bsmap.sh
+++ SeqCap/bsmap.sh
@@ -8,6 +8,21 @@
 #set -xe
 set -xeuo pipefail
 
+echo ------------------------------------------------------
+echo -n 'Job is running on node '; cat $PBS_NODEFILE
+echo ------------------------------------------------------
+echo PBS: qsub is running on $PBS_O_HOST
+echo PBS: originating queue is $PBS_O_QUEUE
+echo PBS: executing queue is $PBS_QUEUE
+echo PBS: working directory is $PBS_O_WORKDIR
+echo PBS: execution mode is $PBS_ENVIRONMENT
+echo PBS: job identifier is $PBS_JOBID
+echo PBS: job name is $PBS_JOBNAME
+echo PBS: node file is $PBS_NODEFILE
+echo PBS: current home directory is $PBS_O_HOME
+echo PBS: PATH = $PBS_O_PATH
+echo ------------------------------------------------------
+
 cd "$PBS_O_WORKDIR"
 
 #reference file dir
