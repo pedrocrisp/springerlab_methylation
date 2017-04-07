6239f17f329fc25a657a20a9a9233e90e5519d51
pedrocrisp <pedrocrisp@gmail.com>
2017-04-07 14:35:16 -0500

    gunzip manually for trim_galore

--- SeqCap/01-trim_galore.sh
+++ SeqCap/01-trim_galore.sh
@@ -49,10 +49,20 @@ mkdir -p $trimmedfolder
 fastqcfolder=analysis/fastqc
 mkdir -p $fastqcfolder
 
+#uncompress reads because trim_galore throws the error `gzip: stdout: Broken pipe` if I input .gz files
+gunzip reads/${ID}_R1_001.fastq.gz
+gunzip reads/${ID}_R2_001.fastq.gz
+
 ########## Run #################
 trim_galore \
 --phred33 \
 --fastqc \
 --gzip \
 --fastqc_args "--noextract --outdir $fastqcfolder" \
--o $trimmedfolder --paired reads/${ID}_R1_001.fastq.gz reads/${ID}_R2_001.fastq.gz
+-o $trimmedfolder --paired reads/${ID}_R1_001.fastq reads/${ID}_R2_001.fastq
+
+#compress original reads again
+gzip reads/${ID}_R1_001.fastq
+gzip reads/${ID}_R2_001.fastq
+
+echo Done trimming
