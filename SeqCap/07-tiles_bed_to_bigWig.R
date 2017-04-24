42cc27ae635e2954c687134cbb423411249bb3d5
pedrocrisp <pedrocrisp@gmail.com>
2017-04-24 17:23:40 -0500

    SeqCap/07-tiles_bed_to_bigWig.R

---
+++ SeqCap/07-tiles_bed_to_bigWig.R
@@ -0,0 +1,59 @@
+#!/usr/bin/Rscript
+##########
+
+#Peter Crisp
+#2017-04-08
+#R script to 100bp tile data with incorrect chr end tiles
+# because 100bp runs past the end of the chr in most cases
+
+#Sample selection #####
+args <- commandArgs(trailingOnly=TRUE)
+print(args)
+# trailingOnly=TRUE means that only your arguments are returned
+sample <- args[1]
+sample
+data_folder <- args[2]
+data_folder
+reference_tile_file <- args[3]
+reference_tile_file
+
+###########################
+#setup
+library(tidyverse)
+###########################
+
+#reference used to make amendments
+reference_tiles <- read_delim(reference_tile_file, delim ="\t")
+
+#########
+#fix bed CG
+
+broken_bedGraph <- read_delim(paste0(data_folder, "/", sample, "_BSMAP_out.txt.100.CG.sorted.bg"), delim ="\t")
+colnames(broken_bedGraph) <- c("chr", "start", "broken_end", "ratio")
+
+fixed_bedGraph <-
+merge(broken_bedGraph, reference_tiles, by = c("chr", "start"))  %>% select(chr, start, end, ratio)
+
+write_delim(fixed_bedGraph, paste0(data_folder, "/", sample, "_BSMAP_out.txt.100.CG.sorted.fixed.bg"), delim ="\t")
+
+#########
+#fix bed CHG
+
+broken_bedGraph <- read_delim(paste0(data_folder, "/", sample, "_BSMAP_out.txt.100.CHG.sorted.bg"), delim ="\t")
+colnames(broken_bedGraph) <- c("chr", "start", "broken_end", "ratio")
+
+fixed_bedGraph <-
+merge(broken_bedGraph, reference_tiles, by = c("chr", "start"))  %>% select(chr, start, end, ratio)
+
+write_delim(fixed_bedGraph, paste0(data_folder, "/", sample, "_BSMAP_out.txt.100.CHG.sorted.fixed.bg"), delim ="\t")
+
+#########
+#fix bed CHH
+
+broken_bedGraph <- read_delim(paste0(data_folder, "/", sample, "_BSMAP_out.txt.100.CHH.sorted.bg"), delim ="\t")
+colnames(broken_bedGraph) <- c("chr", "start", "broken_end", "ratio")
+
+fixed_bedGraph <-
+merge(broken_bedGraph, reference_tiles, by = c("chr", "start"))  %>% select(chr, start, end, ratio)
+
+write_delim(fixed_bedGraph, paste0(data_folder, "/", sample, "_BSMAP_out.txt.100.CHH.sorted.fixed.bg"), delim ="\t")
