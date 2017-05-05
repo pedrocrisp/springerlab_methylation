6a99678687ae0fa3585b165e75fbb687b4316700
pedrocrisp <pedrocrisp@gmail.com>
2017-05-05 12:59:30 -0500

    zero based BED output

--- SeqCap/met_context_window.pl
+++ SeqCap/met_context_window.pl
@@ -1,14 +1,14 @@
 #!/bin/perl
 #source Qing
 #updated by pete 2017-04-17
+#output is a proper zero based BED file per context
+#each context has the same windows, so long as one of the context has data
+#windows without data in any context are omited
 
 my $INFILE = $ARGV[0];
 my $WINDOW = $ARGV[1] || 100;
 
 # perl ./met_context_window_lin.pl example_input.txt 100
-
-# HWI-ST261:400:B0A1EABXX:8:2208:11321:57205 1:N:0:ATCACG	-	7	16761635	1	x
-# HWI-ST261:400:B0A1EABXX:8:2103:16659:181377 1:N:0:ATCACG	+	1	248757208	X
 
 my %dat;
 my $CGOUTFILE = "$INFILE.$WINDOW.CG.bed";
@@ -38,13 +38,6 @@ while (my $l = <IN>) {
 	my $ctcnt = $f[8];
 	my $context = $f[4];
 
-	#Set bins, dealing with corner case where data at position 1 then the window breaks = 0/window = null?
-#if($pos > 1){
-#	my $bin = int(($pos-1 )/ $WINDOW);
-#}else{
-#	my $bin = 0;
-#}
-
 	my $bin = int(($pos-1 )/ $WINDOW);
 
 	#$dat{$chr}->{$bin}->{'total'}++;
@@ -60,7 +53,7 @@ print STDERR "Printing out window file...\n";
 $ts = localtime();
 print STDERR $ts, "\n";
 
-# Emit window bigBed
+# Emit window BED file (base zero coordinates)
 print STDERR "Printout time\n";
 for my $C (sort {$a <=> $b} keys %dat) {
 
@@ -68,24 +61,24 @@ for my $C (sort {$a <=> $b} keys %dat) {
 		# CG output
 		if($dat{$C}->{$W}->{'CG'}->{'CT'}!=0){
 			my $cgfrac = ($dat{$C}->{$W}->{'CG'}->{'C'} / $dat{$C}->{$W}->{'CG'}->{'CT'});
-			print CGOUT $C, "\t", (($W * $WINDOW) + 1), "\t", (($W + 1) * $WINDOW) , "\t", $dat{$C}->{$W}->{'CG'}->{'sites'}, "\t", $dat{$C}->{$W}->{'CG'}->{'C'}, "\t", $dat{$C}->{$W}->{'CG'}->{'CT'}, "\t", $cgfrac, "\n";
+			print CGOUT $C, "\t", ($W * $WINDOW), "\t", (($W + 1) * $WINDOW) , "\t", $dat{$C}->{$W}->{'CG'}->{'sites'}, "\t", $dat{$C}->{$W}->{'CG'}->{'C'}, "\t", $dat{$C}->{$W}->{'CG'} ->{'CT'}, "\t", $cgfrac, "\n";
 		}else{
-			print CGOUT $C, "\t", (($W * $WINDOW) + 1), "\t", (($W + 1) * $WINDOW) , "\t", $dat{$C}->{$W}->{'CG'}->{'sites'}, "\t", $dat{$C}->{$W}->{'CG'}->{'C'}, "\t", $dat{$C}->{$W}->{'CG'}->{'CT'}, "\tNA\n";
+			print CGOUT $C, "\t", ($W * $WINDOW), "\t", (($W + 1) * $WINDOW) , "\t", $dat{$C}->{$W}->{'CG'}->{'sites'}, "\t", $dat{$C}->{$W}->{'CG'}->{'C'}, "\t", $dat{$C}->{$W}->{'CG'}->{'CT'}, "\tNA\n";
 		}
 		# CHG output
 		if($dat{$C}->{$W}->{'CHG'}->{'CT'}!=0){
 			my $chgfrac = ($dat{$C}->{$W}->{'CHG'}->{'C'} / $dat{$C}->{$W}->{'CHG'}->{'CT'});
-			print CHGOUT $C, "\t", (($W * $WINDOW) + 1), "\t", (($W + 1) * $WINDOW) , "\t", $dat{$C}->{$W}->{'CHG'}->{'sites'}, "\t", $dat{$C}->{$W}->{'CHG'}->{'C'}, "\t", $dat{$C}->{$W}->{'CHG'}->{'CT'}, "\t", $chgfrac, "\n";
+			print CHGOUT $C, "\t", ($W * $WINDOW), "\t", (($W + 1) * $WINDOW) , "\t", $dat{$C}->{$W}->{'CHG'}->{'sites'}, "\t", $dat{$C}->{$W}->{'CHG'}->{'C'}, "\t", $dat{$C}->{$W}->{'CHG'}->{'CT'}, "\t", $chgfrac, "\n";
 		}else{
-			print CHGOUT $C, "\t", (($W * $WINDOW) + 1), "\t", (($W + 1) * $WINDOW) , "\t", $dat{$C}->{$W}->{'CHG'}->{'sites'}, "\t", $dat{$C}->{$W}->{'CHG'}->{'C'}, "\t", $dat{$C}->{$W}->{'CHG'}->{'CT'}, "\tNA\n";
+			print CHGOUT $C, "\t", ($W * $WINDOW), "\t", (($W + 1) * $WINDOW) , "\t", $dat{$C}->{$W}->{'CHG'}->{'sites'}, "\t", $dat{$C}->{$W}->{'CHG'}->{'C'}, "\t", $dat{$C}->{$W}->{'CHG'}->{'CT'}, "\tNA\n";
 		}
 
 		# CHH output
 		if($dat{$C}->{$W}->{'CHH'}->{'CT'}!=0){
 			my $chhfrac = ($dat{$C}->{$W}->{'CHH'}->{'C'} / $dat{$C}->{$W}->{'CHH'}->{'CT'});
-			print CHHOUT $C, "\t", (($W * $WINDOW) + 1), "\t", (($W + 1) * $WINDOW) , "\t", $dat{$C}->{$W}->{'CHH'}->{'sites'}, "\t", $dat{$C}->{$W}->{'CHH'}->{'C'}, "\t", $dat{$C}->{$W}->{'CHH'}->{'CT'}, "\t", $chhfrac, "\n";
+			print CHHOUT $C, "\t", ($W * $WINDOW), "\t", (($W + 1) * $WINDOW) , "\t", $dat{$C}->{$W}->{'CHH'}->{'sites'}, "\t", $dat{$C}->{$W}->{'CHH'}->{'C'}, "\t", $dat{$C}->{$W}->{'CHH'}->{'CT'}, "\t", $chhfrac, "\n";
 		}else{
-			print CHHOUT $C, "\t", (($W * $WINDOW) + 1), "\t", (($W + 1) * $WINDOW) , "\t", $dat{$C}->{$W}->{'CHH'}->{'sites'}, "\t", $dat{$C}->{$W}->{'CHH'}->{'C'}, "\t", $dat{$C}->{$W}->{'CHH'}->{'CT'}, "\tNA\n";
+			print CHHOUT $C, "\t", ($W * $WINDOW), "\t", (($W + 1) * $WINDOW) , "\t", $dat{$C}->{$W}->{'CHH'}->{'sites'}, "\t", $dat{$C}->{$W}->{'CHH'}->{'C'}, "\t", $dat{$C}->{$W}->{'CHH'}->{'CT'}, "\tNA\n";
 		}
 
 	}
