e7e600dc2dd0c031505aacee1f30004ac6326d97
pedrocrisp <pedrocrisp@gmail.com>
2017-05-24 16:34:20 -0500

    COMMENTS

--- SeqCap/09-clean-up.sh
+++ SeqCap/09-clean-up.sh
@@ -3,6 +3,8 @@
 #Peter Crisp
 #2017-05-24
 
+#script to purge non-essential files to save space post running the pipeline
+
 # to run (USE CARE UNTESTED)
 # cd <project folder>
 # bash /home/springer/pcrisp/gitrepos/springerlab_methylation/SeqCap/09-clean-up.sh
@@ -22,3 +24,13 @@ rm -rv analysis/bsmapped
 # remove intermediate mapping files
 rm -rv analysis/bsmapped_filtered/*_sorted_MarkDup.bam
 rm -rv analysis/bsmapped_filtered/*_sorted_MarkDup_pairs.bam
+
+##################
+
+# THINGS THAT REMAIN:
+
+# small files/folders
+# The BSMAPratio folder will be large but that is a key output, save on s3 or on home
+# The reads folder should be redundant, but why not push a copy to s3 for backup...
+## reads could be deleted if space is an issue
+#
