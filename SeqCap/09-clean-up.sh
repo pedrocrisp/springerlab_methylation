ccac537e57db5f5787e4e62d500c76810736e421
pedrocrisp <pedrocrisp@gmail.com>
2017-06-29 13:12:30 -0500

    UPDATED

--- SeqCap/09-clean-up.sh
+++ SeqCap/09-clean-up.sh
@@ -12,13 +12,35 @@
 # then to run: (USE CARE UNTESTED)
 # cd <project folder>
 # bash \
-# /home/springer/pcrisp/gitrepos/springerlab_methylation/SeqCap/09-clean-up.sh
-# <destination on home>
+# /home/springer/pcrisp/gitrepos/springerlab_methylation/SeqCap/09-clean-up.sh \
+# <"purge" or "no_purge"> \
+# <"copy_to_home" or "no_copy_to_home">
+# <destination on home> \
+# <"s3" or "no_s3"> \
+# <destination on s3>
 #############
 
 # NOTE THIS FILE NEEDS TO BE UPDATED TO THE CURRENT PIPELINE
 
-home_dir_destination=$1
+purge=$1
+copy_to_home=$2
+home_dir_destination=$3
+copy_to_s3=$4
+s3_bucket=$5
+folder_for_s3_copy=$PWD
+
+echo "Current folder is $PWD it will be $purge"
+echo "Home copy? $copy_to_home"
+echo "Home destionation $home_dir_destination"
+echo "s3 copy? $copy_to_s3"
+echo "s3 destination $s3_bucket"
+echo "folder for copy to s3 $folder_for_s3_copy"
+
+
+
+# purge
+if [ "$purge" == "purge" ]
+then
 
 #removed trimmed fastq files
 rm -rv analysis/trimmed/*.fq
@@ -39,19 +61,13 @@ rm -rv analysis/bsmapped_filtered/*_sorted_MarkDup_pairs.bam
 mkdir -p analysis/HsMetrics_deDups_logs
 mv analysis/bsmapped_filtered/*.txt analysis/HsMetrics_deDups_logs/
 
-##################
+else
 
-# THINGS THAT SHOULD BE KEPT AT LEAST ON S3 that will not be copied to home below:
+  echo "Skiping purge"
+fi
 
-# The "BSMAPratio" folder will be large but that is a key output, save on s3 or on home
-# The "reads" folder should be redundant, but why not push a copy to s3 for backup...
-## reads could be deleted if space is an issue
-# THE "bsmapped_filtered" folder has bams - useful for IGV, it also has
-
-# it is recommended that everything left after the purge above should be copied to s3
-# eg
-# s3cmd sync --verbose SeqCap_2_McGinnis/ s3://springer-pcrisp-seqcap/SeqCap_2_McGinnis/
-# 76 GB took just under 2 hr to sync ultimately
+  if [ "$copy_to_home" == "copy_to_home" ]
+  then
 
 ##################
 # copy the important stuff to backed up file system
@@ -75,3 +91,33 @@ rsync -rhivPt *.txt $home_dir_destination/
 
 # BSMAPratio?
 rsync -rhivPt analysis/BSMAPratio $home_dir_destination/
+
+else
+
+  echo "Skiping copy to home"
+fi
+
+  if [ "$copy_to_s3" == "copy_to_s3" ]
+  then
+
+    ##################
+
+    # THINGS THAT SHOULD BE KEPT AT LEAST ON S3 that will not be copied to home below:
+
+    # The "BSMAPratio" folder will be large but that is a key output, save on s3 or on home
+    # The "reads" folder should be redundant, but why not push a copy to s3 for backup...
+    ## reads could be deleted if space is an issue
+    # THE "bsmapped_filtered" folder has bams - useful for IGV, it also has
+
+    # it is recommended that everything left after the purge above should be copied to s3
+    # eg
+    # s3cmd sync --verbose SeqCap_2_McGinnis/ s3://springer-pcrisp-seqcap/SeqCap_2_McGinnis/
+    # 76 GB took just under 2 hr to sync ultimately
+
+    s3cmd sync --verbose ../$folder_for_s3_copy $s3_bucket
+
+else
+
+  echo "Skiping copy to s3"
+
+  fi
