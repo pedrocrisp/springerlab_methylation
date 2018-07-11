cf1bdd3c51df929bac0e70f77342af03e9748170
pedrocrisp <pedrocrisp@gmail.com>
2018-07-11 09:12:26 -0500

    updates

--- SeqCap/11b-DSS_call_tile_DMRs.R
+++ SeqCap/11b-DSS_call_tile_DMRs.R
@@ -22,6 +22,7 @@ library(DSS)
 library(bsseq)
 library(limma)
 library(tidygenomics)
+library(ggthemes)
 old.scipen <- getOption("scipen")
 options(scipen=999)
 
@@ -55,10 +56,12 @@ context = "CG"
 #######  read in the files
 sample1_data <- read_tsv(file.path(path_to_data_files, paste0(sample1, "_BSMAP_out.txt.100.", context, "_filtered.txt")), col_names = c("chr", "pos", "X", "N", "sites"), skip = 1)
 sample1_data <- sample1_data %>% select("chr", "pos", "N", "X", "sites")
+sample1_data <- sample1_data %>% filter(chr == "Chr01")
 sample1_data
 
 sample2_data <- read_tsv(file.path(path_to_data_files, paste0(sample2, "_BSMAP_out.txt.100.", context, "_filtered.txt")), col_names = c("chr", "pos", "X", "N", "sites"), skip = 1)
 sample2_data <- sample2_data %>% select("chr", "pos", "N", "X", "sites")
+sample2_data <- sample2_data %>% filter(chr == "Chr01")
 sample2_data
 
 #######  1. create an object of BSseq class (requires bsseq Bioconductor package)
@@ -129,6 +132,11 @@ CG_tiles_tested <- dmlTest_calls %>% mutate(tile = paste0(chr, "_", pos)) %>% se
 
 CG_DMRs <- dmls_tbl %>% mutate(tile = paste0(chr, "_", pos), DMR = ifelse(diff > 0, 1, -1)) %>% select(tile, DMR)
 
+CG_mu <- dmls_tbl %>%
+  mutate(tile = paste0(chr, "_", pos), DMR = ifelse(diff > 0, "hyper", "hypo")) %>%
+  mutate(DMR_type = paste0(context, ".", "DMR")) %>%
+  select(tile, DMR_type, mu1, mu2)
+
 #######  #######
 # CHG
 #######  #######
@@ -138,10 +146,12 @@ context = "CHG"
 #######  read in the files
 sample1_data <- read_tsv(file.path(path_to_data_files, paste0(sample1, "_BSMAP_out.txt.100.", context, "_filtered.txt")), col_names = c("chr", "pos", "X", "N", "sites"), skip = 1)
 sample1_data <- sample1_data %>% select("chr", "pos", "N", "X", "sites")
+sample1_data <- sample1_data %>% filter(chr == "Chr01")
 sample1_data
 
 sample2_data <- read_tsv(file.path(path_to_data_files, paste0(sample2, "_BSMAP_out.txt.100.", context, "_filtered.txt")), col_names = c("chr", "pos", "X", "N", "sites"), skip = 1)
 sample2_data <- sample2_data %>% select("chr", "pos", "N", "X", "sites")
+sample2_data <- sample2_data %>% filter(chr == "Chr01")
 sample2_data
 
 #######  1. create an object of BSseq class (requires bsseq Bioconductor package)
@@ -204,6 +214,11 @@ CHG_tiles_tested <- dmlTest_calls %>% mutate(tile = paste0(chr, "_", pos)) %>% s
 
 CHG_DMRs <- dmls_tbl %>% mutate(tile = paste0(chr, "_", pos), DMR = ifelse(diff > 0, 1, -1)) %>% select(tile, DMR)
 
+CHG_mu <- dmls_tbl %>%
+  mutate(tile = paste0(chr, "_", pos), DMR = ifelse(diff > 0, "hyper", "hypo")) %>%
+  mutate(DMR_type = paste0(context, ".", "DMR")) %>%
+  select(tile, DMR_type, mu1, mu2)
+
 #######  #######
 # CHH
 #######  #######
@@ -213,10 +228,12 @@ context = "CHH"
 #######  read in the files
 sample1_data <- read_tsv(file.path(path_to_data_files, paste0(sample1, "_BSMAP_out.txt.100.", context, "_filtered.txt")), col_names = c("chr", "pos", "X", "N", "sites"), skip = 1)
 sample1_data <- sample1_data %>% select("chr", "pos", "N", "X", "sites")
+sample1_data <- sample1_data %>% filter(chr == "Chr01")
 sample1_data
 
 sample2_data <- read_tsv(file.path(path_to_data_files, paste0(sample2, "_BSMAP_out.txt.100.", context, "_filtered.txt")), col_names = c("chr", "pos", "X", "N", "sites"), skip = 1)
 sample2_data <- sample2_data %>% select("chr", "pos", "N", "X", "sites")
+sample2_data <- sample2_data %>% filter(chr == "Chr01")
 sample2_data
 
 #######  1. create an object of BSseq class (requires bsseq Bioconductor package)
@@ -279,12 +296,198 @@ CHH_tiles_tested <- dmlTest_calls %>% mutate(tile = paste0(chr, "_", pos)) %>% s
 
 CHH_DMRs <- dmls_tbl %>% mutate(tile = paste0(chr, "_", pos), DMR = ifelse(diff > 0, 1, -1)) %>% select(tile, DMR)
 
-#######  #######
+CHH_mu <- dmls_tbl %>%
+  mutate(tile = paste0(chr, "_", pos), DMR = ifelse(diff > 0, "hyper", "hypo")) %>%
+  mutate(DMR_type = paste0(context, ".", DMR)) %>%
+  select(tile, DMR_type, mu1, mu2)
+
+#######  ####### #######  ####### #######  ####### #######  ####### #######  ####### #######  #######
 # Some analysis
-#######  #######
+#######  ####### #######  ####### #######  ####### #######  ####### #######  ####### #######  #######
+
+outFolder_analysis <- paste0(outFolder, "/Analysis_", contrast)
+dir.create(outFolder_analysis)
 
 # Overlap between tiles that passed filtering
 
+CG_tiles_tested <- CG_tiles_tested %>% mutate(context = "CG", pass = 1)
+CHG_tiles_tested <- CHG_tiles_tested %>% mutate(context = "CHG", pass = 1)
+CHH_tiles_tested <- CHH_tiles_tested %>% mutate(context = "CHH", pass = 1)
+
+tile_lists <- bind_rows(CG_tiles_tested, CHG_tiles_tested, CHH_tiles_tested)
+
+table_venn <- tile_lists %>%
+  mutate(pass = as.integer(pass)) %>%
+  spread(key = context, value = pass, fill = 0)
+
+table_venn_summary <- table_venn %>%
+  group_by(CG, CHG, CHH) %>%
+  summarise(Counts = n()) %>%
+  ungroup() %>%
+  mutate(tiles_pct = round(Counts/sum(Counts)*100, digits = 1))
+
+table_venn_summary
+
+ref_venn <- as.tibble(expand.grid(0:1, 0:1, 0:1)) %>% mutate(Counts = 0, tiles_pct = 0)
+colnames(ref_venn) <- colnames(table_venn_summary)
+
+ref_venn
+
+table_venn_summary_all <- ref_venn %>% anti_join(table_venn_summary, by = c(colnames(table_venn_summary)[1:3])) %>% bind_rows(table_venn_summary)
+
+table_venn_summary_counts <- table_venn_summary_all %>%
+  select(-tiles_pct)
+
+venn_data <- as.matrix(table_venn_summary_counts)
+class(venn_data) <- "VennCounts"
+venn_data
+
+pdf(paste0(outFolder_analysis, "/", contrast,"_tiles_assessed_context_overlap.pdf"), width = 4, height = 4)
+vennDiagram(venn_data,
+            main= "Tiles assessed per context",
+            # include=c("down"),
+            # counts.col=c('blue'),
+            show.include=T,
+            cex=c(0.75)
+            # names=c("Excess-light 1 hour","Drought 7 days")
+)
+dev.off()
+
+table_venn_summary_2 <- table_venn_summary_all %>%
+  mutate(Counts = tiles_pct) %>%
+  select(-tiles_pct)
+
+venn_data <- as.matrix(table_venn_summary_2)
+class(venn_data) <- "VennCounts"
+venn_data
+
+pdf(paste0(outFolder_analysis, "/", contrast,"_tiles_assessed_context_overlap_pct.pdf"), width = 4, height = 4)
+vennDiagram(venn_data,
+            main= "Tiles assessed per context",
+            # include=c("down"),
+            # counts.col=c('blue'),
+            show.include=T,
+            cex=c(0.75)
+            # names=c("Excess-light 1 hour","Drought 7 days")
+)
+dev.off()
+
 # Overlap between DMRs
 
+CG_DMRs <- CG_DMRs %>% mutate(context = "CG")
+CHG_DMRs <- CHG_DMRs %>% mutate(context = "CHG")
+CHH_DMRs <- CHH_DMRs %>% mutate(context = "CHH")
 
+tile_lists <- bind_rows(CG_DMRs, CHG_DMRs, CHH_DMRs)
+tile_lists
+
+table_venn <- tile_lists %>%
+  mutate(DMR = as.integer(DMR)) %>%
+  spread(key = context, value = DMR, fill = 0)
+
+venn_data <- vennCounts(table_venn[,-1], include=c("both"))
+venn_data
+
+pdf(paste0(outFolder_analysis, "/", contrast,"_DMRs_context_overlap_both.pdf"), width = 4, height = 4)
+vennDiagram(venn_data,
+            main= "all DMRs per context",
+            # include=c("down"),
+            # counts.col=c('blue'),
+            show.include=T,
+            cex=c(0.75)
+            # names=c("Excess-light 1 hour","Drought 7 days")
+)
+
+vennDiagram(venn_data, names = "",
+            # main= "hyper DMRs per context",
+            # include=c("down"),
+            # counts.col=c('blue'),
+            show.include=T,
+            cex=c(0.75)
+            # names=c("Excess-light 1 hour","Drought 7 days")
+)
+dev.off()
+
+
+venn_data <- vennCounts(table_venn[,-1], include=c("up"))
+venn_data
+pdf(paste0(outFolder_analysis, "/", contrast,"_DMRs_context_overlap_up.pdf"), width = 4, height = 4)
+vennDiagram(venn_data,
+            main= "hyper DMRs per context",
+            include=c("up"),
+            counts.col=c('red'),
+            show.include=T,
+            cex=c(0.75)
+            # names=c("Excess-light 1 hour","Drought 7 days")
+)
+
+vennDiagram(venn_data, names = "",
+            # main= "hyper DMRs per context",
+            include=c("up"),
+            counts.col=c('red'),
+            show.include=T,
+            cex=c(0.75)
+            # names=c("Excess-light 1 hour","Drought 7 days")
+)
+dev.off()
+
+venn_data <- vennCounts(table_venn[,-1], include=c("down"))
+venn_data
+pdf(paste0(outFolder_analysis, "/", contrast,"_DMRs_context_overlap_down.pdf"), width = 4, height = 4)
+vennDiagram(venn_data,
+            main= "hypo DMRs per context",
+            include=c("down"),
+            counts.col=c('blue'),
+            show.include=T,
+            cex=c(0.75)
+            # names=c("Excess-light 1 hour","Drought 7 days")
+)
+
+vennDiagram(venn_data, names = "",
+            # main= "hypo DMRs per context",
+            include=c("down"),
+            counts.col=c('blue'),
+            show.include=T,
+            cex=c(0.75)
+            # names=c("Excess-light 1 hour","Drought 7 days")
+)
+dev.off()
+
+venn_data <- vennCounts(table_venn[,-1], include=c("both"))
+venn_data
+
+venn_data_2 <- venn_data
+class(venn_data_2) <- "matrix"
+
+venn_data_pct <- as.tibble(venn_data_2) %>%
+  mutate(Counts = round(Counts/sum(Counts)*100, digits = 1))
+
+venn_data_pct
+
+venn_data <- as.matrix(venn_data_pct)
+class(venn_data) <- "VennCounts"
+venn_data
+
+pdf(paste0(outFolder_analysis, "/", contrast,"_DMRs_context_overlap_both_pct.pdf"), width = 4, height = 4)
+vennDiagram(venn_data,
+            main= "DMRs per context",
+            # include=c("down"),
+            # counts.col=c('blue'),
+            show.include=T,
+            cex=c(0.75)
+            # names=c("Excess-light 1 hour","Drought 7 days")
+)
+dev.off()
+
+###### Density plot of mC levels for DMRs
+
+DMR_lists <- bind_rows(CG_mu, CHG_mu, CHH_mu) %>% gather(key = sample, value = mC, -DMR_type, -tile)
+DMR_lists
+
+pdf(paste0(outFolder_analysis, "/", contrast,"_DMRs_mC_values_distribution.pdf"), width = 6, height = 4)
+ggplot(DMR_lists, aes(mC, colour = DMR_type)) +
+  geom_density() +
+  theme_minimal() +
+  labs(title = "DMRs_mC_values_distribution") +
+  scale_color_ptol()
+dev.off()
