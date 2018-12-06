0f9f8b86d3a671ec369be5cb989fe0d2c5ca72bd
pedrocrisp <pedrocrisp@gmail.com>
2018-12-06 11:42:54 -0600

    indentation issues

--- SeqCap/01-trim_galore_swift.sh
+++ SeqCap/01-trim_galore_swift.sh
@@ -52,49 +52,49 @@ mkdir -p $fastqcfolder
 # check if single or paired end by looking for R2 file
 if [ -e "reads/${ID}_R2_001.fastq.gz" ]; then
 
-    echo "paired reads"
+echo "paired reads"
 
-    #uncompress reads because trim_galore throws the error `gzip: stdout: Broken pipe` if I input .gz files
-    gunzip reads/${ID}_R1_001.fastq.gz
-    gunzip reads/${ID}_R2_001.fastq.gz
+#uncompress reads because trim_galore throws the error `gzip: stdout: Broken pipe` if I input .gz files
+gunzip reads/${ID}_R1_001.fastq.gz
+gunzip reads/${ID}_R2_001.fastq.gz
 
-    ########## Run #################
-    # for swift libraries this trimms 20bp from the 5' end of the R2 read to remove the adaptase tail.
-    # Swift recommends symetrical trimming, so I trim from the R1 read too...
+########## Run #################
+# for swift libraries this trimms 20bp from the 5' end of the R2 read to remove the adaptase tail.
+# Swift recommends symetrical trimming, so I trim from the R1 read too...
 
-    trim_galore \
-    --phred33 \
-    --fastqc \
-    --fastqc_args "--noextract --outdir $fastqcfolder" \
-    --clip_R1 20 \
-    --clip_R2 20 \
-    -o $trimmedfolder --paired reads/${ID}_R1_001.fastq reads/${ID}_R2_001.fastq
+trim_galore \
+--phred33 \
+--fastqc \
+--fastqc_args "--noextract --outdir $fastqcfolder" \
+--clip_R1 20 \
+--clip_R2 20 \
+-o $trimmedfolder --paired reads/${ID}_R1_001.fastq reads/${ID}_R2_001.fastq
 
-    #compress original reads again
-    gzip reads/${ID}_R1_001.fastq
-    gzip reads/${ID}_R2_001.fastq
+#compress original reads again
+gzip reads/${ID}_R1_001.fastq
+gzip reads/${ID}_R2_001.fastq
 
 else
-    echo "assuming single end"
+echo "assuming single end"
 
-    #uncompress reads because trim_galore throws the error `gzip: stdout: Broken pipe` if I input .gz files
-    gunzip reads/${ID}_R1_001.fastq.gz
+#uncompress reads because trim_galore throws the error `gzip: stdout: Broken pipe` if I input .gz files
+gunzip reads/${ID}_R1_001.fastq.gz
 
-    ########## Run #################
-    # for swift libraries this trimms 20bp from the 5' end of the R2 read to remove the adaptase tail.
-    # Swift recommends symetrical trimming, so I trim from the R1 read too...
+########## Run #################
+# for swift libraries this trimms 20bp from the 5' end of the R2 read to remove the adaptase tail.
+# Swift recommends symetrical trimming, so I trim from the R1 read too...
 
-    trim_galore \
-    --phred33 \
-    --fastqc \
-    --fastqc_args "--noextract --outdir $fastqcfolder" \
-    --clip_R1 20 \
-    --clip_R2 20 \
-    -o $trimmedfolder \
-    reads/${ID}_R1_001.fastq
+trim_galore \
+--phred33 \
+--fastqc \
+--fastqc_args "--noextract --outdir $fastqcfolder" \
+--clip_R1 20 \
+--clip_R2 20 \
+-o $trimmedfolder reads/${ID}_R1_001.fastq
 
-    #compress original reads again
-    gzip reads/${ID}_R1_001.fastq
+#compress original reads again
+gzip reads/${ID}_R1_001.fastq
+
 fi 
 
 echo Done trimming
