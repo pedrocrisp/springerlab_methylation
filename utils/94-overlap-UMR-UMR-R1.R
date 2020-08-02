5afda4da23955dac64e0c6b9bfe4177c6c0b9444
Peter Crisp <pedrocrisp@gmail.com>
2020-08-02 10:04:31 +1000

    94-umr overlaps

---
+++ utils/94-overlap-UMR-UMR-R1.R
@@ -0,0 +1,83 @@
+#!/usr/bin/Rscript
+##########
+
+#Peter Crisp
+#2020-02-08
+#R script to overlap UMRs files
+
+# Notes
+# post filtering may be required to remove organelles or other undesired contigs
+
+#Sample selection #####
+args <- commandArgs(trailingOnly=TRUE)
+print(args)
+# trailingOnly=TRUE means that only your arguments are returned
+# sample_name <- args[1]
+inputFile <- args[1]
+summary_output_folder <- args[2]
+
+######## de bug
+# args
+# inputFile <- "Sorghum_7days_leaf_ACRs_meth.bed"
+# summary_output_folder="~/ws/analysis/ongoing/mC_sorghum/analysis/Sbicolor_SRR3286309_mC_domains_II_cov_3_sites_2_MR_0.4_UMR_0.1/mC_UMT_annotation"
+#
+
+########
+###########################
+library(tidyverse)
+library(ggthemes)
+# library("seqinr")
+old.scipen <- getOption("scipen")
+options(scipen=999)
+# library(wesanderson)
+library(RColorBrewer)
+
+text_size_theme_8 <- theme(axis.text=element_text(size=8),
+                           axis.title=element_text(size=8),
+                           axis.text.x=element_text(angle = 45, hjust = 1),
+                           legend.title=element_text(size=8),
+                           legend.text=element_text(size=8))
+
+###########################
+# Module #1
+###########################
+
+overlaps <- read_tsv(paste0("tmp_", inputFile, ".bed"),
+                     col_names = c("chr", "start", "end", "summit", "location", "cat",
+                                   "b_chr", "b_start", "b_end", "b_feature", "b_ID", "b_optional", "distance"),
+                     cols(chr = col_character(),
+                          # Olap_file = col_character(),
+                          b_chr = col_character()))
+
+overlaps
+# 34,514 (acrs 32,481) - only a few extra multi overlaps to resolve!
+
+# resovle multi-overlaps
+# I think most of the multi-overlaps will be at distance zero - this pipeline is mainly aimed at resolving that. Its possible some will be due to a peak being exactally the same length between an upstrea and downstream gene, but I might just pick one for these because I think they will be rare
+# get distinct tile + feature rows
+overlaps_distinct_collapsed <- overlaps %>%
+  mutate(methylation = ifelse(distance == 0, "shared_UMR", "unique_UMR")) %>%
+  select(chr, start, end, summit, location, cat, methylation) %>%
+  distinct()
+# 32,620
+
+overlaps_distinct_collapsed %>% group_by(chr, start) %>% summarise(n = n()) %>% group_by(n) %>% summarise(n2 = n())
+
+overlaps_distinct_collapsed
+# 21,267
+
+write.table(overlaps_distinct_collapsed, paste0(inputFile, ".bed"), sep = "\t", quote = F, row.names = F, col.names = F)
+
+overlaps_distinct_collapsed_filtered_summary <- overlaps_distinct_collapsed %>%
+  group_by(methylation) %>%
+  summarise(n = n()) %>%
+  mutate(percentage = n/sum(n)*100)
+
+overlaps_distinct_collapsed_filtered_summary
+# # A tibble: 2 x 3
+#   methylation     n percentage
+#   <chr>       <int>      <dbl>
+# 1 non_UMR      1804       5.62
+# 2 UMR         30307      94.4
+
+write_tsv(overlaps_distinct_collapsed_filtered_summary, paste0(summary_output_folder, "/", inputFile, "_summary.tsv"))
---
+++ utils/94-overlap-UMR-UMR.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+#Set -e as an option, it tells the command line to exit the script immediately if it registers an error.
+set -e
+#Set -x as an option, it tells the computer to echo back each step before it is completed and the output is produced.
+set -x
+
+# This script overlaps a umr file with another UMR file
+# UMRs are annotated as inaccessible iUMR and accessible aUMRs and the total of each reported in the summary file
+# Overlap include min 1bp overlap
+
+### Inputs
+# The input bed files should be 6 columns
+# Column1: chromosome
+# Column2: start
+# Column3: stop
+# Column4: name (or other desired info) eg UMR or ACR
+# Column5: optional eg ID eg UMR_21
+# Column6: optional eg size
+
+# Output
+# Column1: chromosome
+# Column2: start
+# Column3: stop
+# Column4: name (or other desired info) eg UMR or ACR
+# Column5: optional eg ID eg UMR_21
+# Column6: optional eg size
+# Column7: annotation eg i/aUMR or m/uACR
+
+###
+#code to make script work on both osx and linux.
+
+if
+[[ $OSTYPE == darwin* ]]
+then
+readlink=$(which greadlink)
+scriptdir="$(dirname $($readlink -f $0))"
+else
+scriptdir="$(dirname $(readlink -f $0))"
+fi
+#
+
+########## Args #################
+# sample_name=$1
+umr_bed_file=$1
+acr_bed_file=$2
+chrom_sizes_file=$3
+summary_output_folder=$4
+
+# debug
+# cd ~/ws/analysis/ongoing/mC_sorghum/analysis/Sbicolor_SRR3286309_mC_domains_II_cov_3_sites_2_MR_0.4_UMR_0.1/mC_UMT_annotation_beds
+# sample_name=Sbicolor_SRR3286309
+# umr_bed_file=Sbicolor_SRR3286309_UMRs_6col.bed
+# acr_bed_file=~/ws/analysis/ongoing/mC_sorghum/Sorghum_7days_leaf_ACRs.bed
+# chrom_sizes_file=~/ws/refseqs/sorghum/Sbicolor_454_v3.0.1_sorted.chrom.sizes
+# summary_output_folder=~/ws/analysis/ongoing/mC_sorghum/analysis/Sbicolor_SRR3286309_mC_domains_II_cov_3_sites_2_MR_0.4_UMR_0.1/mC_UMT_annotation
+
+tmp_umr_sample_prefix="$(basename $umr_bed_file)"
+umr_sample_prefix="${tmp_umr_sample_prefix%%.*}"
+
+tmp_acr_sample_prefix="$(basename $acr_bed_file)"
+acr_sample_prefix="${tmp_acr_sample_prefix%%.*}"
+
+# optput file names
+umr_outputFile=${umr_sample_prefix}_olap_${acr_sample_prefix}
+echo $umr_outputFile
+acr_outputFile=${acr_sample_prefix}_olap_${umr_sample_prefix}
+echo $acr_outputFile
+
+#basename
+#umr_sample_prefix="$(basename $umr_bed_file)"
+#echo $umr_sample_prefix
+
+#umr_outputFile="${umr_sample_prefix%%.*}_access"
+#echo $umr_outputFile
+
+#acr_sample_prefix="$(basename $acr_bed_file)"
+#echo $acr_sample_prefix
+
+#acr_outputFile="${acr_sample_prefix%%.*}_meth"
+#echo $acr_outputFile
+
+mkdir -p logs
+
+########## Modules #################
+
+module load R/3.3.2
+module load bedtools
+
+########## overlaps ACR annotation #################
+
+# ACRs regions that overlap UMRs
+bedtools closest \
+-a $acr_bed_file \
+-b $umr_bed_file \
+-mdb all \
+-t all \
+-D b \
+-g $chrom_sizes_file \
+> tmp_${acr_outputFile}.bed
+
+########## R module to parse ACR annotation output #################
+R -f ~/gitrepos/springerlab_methylation/utils/94-overlap-UMR-UMR-R1.R \
+--args $acr_outputFile $summary_output_folder
+
+########## overlaps UMR annotation #################
+
+# UMRs that overlap ACRs
+bedtools closest \
+-a $umr_bed_file \
+-b $acr_bed_file \
+-mdb all \
+-t all \
+-D b \
+-g $chrom_sizes_file \
+> tmp_${umr_outputFile}.bed
+
+########## R module to parse UMR annotation output #################
+R -f ~/gitrepos/springerlab_methylation/utils/94-overlap-UMR-UMR-R2.R \
+--args $umr_outputFile $summary_output_folder
+
+# to run
+# bash ~/gitrepos/springerlab_methylation/utils/91-overlap-UMR-ACR.sh \
+# Sbicolor_SRR3286309_UMRs_6col.bed \
+# ~/ws/analysis/ongoing/mC_sorghum/Sorghum_7days_leaf_ACRs.bed \
+# ~/ws/refseqs/sorghum/Sbicolor_454_v3.0.1_sorted.chrom.sizes \
+# ~/ws/analysis/ongoing/mC_sorghum/analysis/Sbicolor_SRR3286309_mC_domains_II_cov_3_sites_2_MR_0.4_UMR_0.1/mC_UMT_annotation
+# >> \
+# logs/$(date +%Y%m%d-%H%M%S)_UMR_ACR_overlap.log 2>&1
