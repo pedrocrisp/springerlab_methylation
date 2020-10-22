27d5b68a91a92d007ae61ac362474f15e33fa97d
Peter Crisp <pedrocrisp@gmail.com>
2020-10-22 21:26:14 +1000

    count CG etc sites in any bed file

---
+++ utils/create_genome_tile_mC_counts_2-qsub.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#set -xe
+set -xeuo pipefail
+
+#Peter Crisp
+#2019-12-12
+#Bash qsub script for create_genome_tile_mC_counts.R
+
+usage="USAGE:
+create_genome_tile_mC_counts-qsub <sample_list> <genome_prefix> <walltime> <memory>
+"
+
+#define stepo in the pipeline - should be the same name as the script
+step=create_genome_tile_mC_counts_2
+
+######### Setup ################
+sample_list=$1
+genome_prefix=$2
+walltime=$3
+mem=$4
+
+if [ "$#" -lt "4" ]
+then
+echo $usage
+exit -1
+else
+echo "Submitting samples listed in '$sample_list' for analysis"
+cat $sample_list
+fi
+
+#number of samples
+number_of_samples=`wc -l $sample_list | awk '{print $1}'`
+if [[ "$number_of_samples" -eq 1 ]]
+then
+qsub_t=1
+else
+qsub_t="1-${number_of_samples}"
+fi
+echo "argument to be passed to qsub -t is '$qsub_t'"
+
+#find script to run, makes it file system agnostic
+if
+[[ $OSTYPE == darwin* ]]
+then
+readlink=$(which greadlink)
+scriptdir="$(dirname $($readlink -f $0))"
+else
+scriptdir="$(dirname $(readlink -f $0))"
+fi
+
+########## Run #################
+
+#make log and analysis folders
+#make logs folder if it doesnt exist yet
+mkdir -p logs
+
+timestamp=$(date +%Y%m%d-%H%M%S)
+
+#make analysis dir if it doesnt exist yet
+analysis_dir=analysis
+mkdir -p $analysis_dir
+
+#make trimmgalore logs folder, timestamped
+log_folder=logs/${timestamp}_${step}
+mkdir $log_folder
+
+#script path and cat a record of what was run
+script_to_qsub=${scriptdir}/${step}.sh
+cat $script_to_qsub > ${log_folder}/script.log
+cat $0 > ${log_folder}/qsub_runner.log
+
+#submit qsub and pass args
+#-o and -e pass the file locations for std out/error
+#-v additional variables to pass to the qsub script including the PBS_array list and the dir structures
+qsub -t $qsub_t \
+-l walltime=${walltime},nodes=1:ppn=2,mem=${mem}gb \
+-o ${log_folder}/${step}_o \
+-e ${log_folder}/${step}_e \
+-v LIST=${sample_list},genome_prefix=$genome_prefix \
+$script_to_qsub
---
+++ utils/create_genome_tile_mC_counts_2_tiles.R
@@ -0,0 +1,82 @@
+#!/usr/bin/Rscript
+##########
+
+#Peter Crisp
+#2019-12-12
+#R script to parse bed interval file for sites counting
+
+#Sample selection #####
+args <- commandArgs(trailingOnly=TRUE)
+print(args)
+# trailingOnly=TRUE means that only your arguments are returned
+genome <- args[1]
+genome
+
+###########################
+#setup
+library(data.table)
+library(tidyverse)
+old.scipen <- getOption("scipen")
+options(scipen=999)
+###########################
+
+# read in bed file
+reference_tiles <- <- read_tsv(paste0("../", genome, ".bed"), col_names = c("chr", "start", "end"))
+reference_tiles_2 <- reference_tiles %>%
+  mutate(start_zBased = start-1)
+
+reference_tiles_2
+
+# # A tibble: 7,097,206 x 4
+#    chr   start   end start_zBased
+#    <chr> <dbl> <dbl>        <dbl>
+#  1 Chr11     1   100            0
+#  2 Chr11   101   200          100
+#  3 Chr11   201   300          200
+#  4 Chr11   301   400          300
+#  5 Chr11   401   500          400
+#  6 Chr11   501   600          500
+#  7 Chr11   601   700          600
+#  8 Chr11   701   800          700
+#  9 Chr11   801   900          800
+# 10 Chr11   901  1000          900
+
+# write output using write.table because write_tsv coerces to scientific notation
+write.table(reference_tiles_2, paste0(genome, "_100bp_tiles_zBased.txt"),  sep = "\t", quote = F, row.names = F)
+
+# write out as a bed file too
+reference_tiles_bed <- reference_tiles_2 %>%
+  mutate(start = start_zBased) %>%
+  select(chr, start, end) %>%
+  mutate(name = paste0(chr, ":", start, "-", end))
+
+#awk '{ if (NR==1622117) print $0 }' GDDH13_1-1_formatted_Pt_100pb_tiles_for_sites_calc.bed
+
+# again use write.table
+write.table(reference_tiles_bed, paste0(genome, "_100bp_tiles.bed"),  sep = "\t", quote = F, row.names = F, col.names = F)
+
+
+############ 100 bp tile reference for calling sites (+2nt) #############
+
+# tiles <- read_tsv("../ZmaysPH207_443_v1_0_100bp_tiles_zBased.txt", col_names = F,
+#                   cols_only(
+#                     X1 = col_character(),
+#                     X2 = col_integer(),
+#                     X3 = col_integer()
+#                   ))
+
+reference_tiles_2 %>% distinct(chr)
+
+tiles_for_sites <- reference_tiles_2 %>%
+  # slice(1:10) %>%
+  group_by(chr) %>%
+  mutate(new_start = ifelse(start_zBased >= 2, start_zBased - 2, 0),
+         new_end = ifelse(end < max(end), end + 2, max(end)))
+
+tiles_for_sites %>% filter(start == min(start) | end == max(end))
+
+tiles_for_sites_slim <- tiles_for_sites %>% select(chr, new_start, new_end)
+
+tiles_for_sites_slim %>% distinct(chr)
+
+write.table(tiles_for_sites_slim, paste0(genome, "_100pb_tiles_for_sites_calc.bed"), sep = "\t", quote = F, row.names = F, col.names = F)
